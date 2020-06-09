cb47e59b8e4dc83198d9a1220adbb45e29f949ee
copygirl <copygirl@mcft.net>
2020-06-09 15:26:16 +0200

    Bump version to 0.2.0

--- packs/obsidian12pack.voodoo.kts
+++ packs/obsidian12pack.voodoo.kts
@@ -1,6 +1,6 @@
 title     = "obsidian 1.12 Modpack"
 authors   = listOf("copygirl", "Nikky")
-version   = "0.1.1"
+version   = "0.2.0"
 icon      = rootFolder.resolve("icon.png")
 
 mcVersion = "1.12.2"
