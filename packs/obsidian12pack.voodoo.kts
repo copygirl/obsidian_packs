442940b139a4b2a1aac47ae257a9189935e7b066
copygirl <copygirl@mcft.net>
2020-06-10 18:57:14 +0200

    Bump version to 0.2.4
    Removed mods:
    - MAGE, as it causes a render exception

--- packs/obsidian12pack.voodoo.kts
+++ packs/obsidian12pack.voodoo.kts
@@ -1,6 +1,6 @@
 title     = "obsidian 1.12 Modpack"
 authors   = listOf("copygirl", "Nikky")
-version   = "0.2.3"
+version   = "0.2.4"
 icon      = rootFolder.resolve("icon.png")
 
 mcVersion = "1.12.2"
@@ -191,7 +191,6 @@ root<Curse> {
                 +Mod.betterFoliage   { description = "Prettier vegetation, such as leaves and grass, at some FPS cost" }
                 +Mod.betterPlacement { description = "Don't skip blocks when holding down mouse button to place" }
                 +Mod.itemScroller    { description = "Use scroll wheel and other shortcuts to move items" }
-                +Mod.mage            { description = "Primarily fixes smooth lighting in and around water" }
                 +Mod.mumblelink      { description = "Links Minecraft to Mumble, enabling positional audio" }
             }
         }
